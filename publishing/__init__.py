d5030690f93c0b1288a6ddd75625ad5f8684dbb8
Carlos <niveando@yahoo.com>
2020-04-08 18:33:49 +0200

    CML: Añadida la parte de publicación en Zenodo

---
+++ publishing/__init__.py
@@ -0,0 +1,71 @@
+import os
+import requests
+import ntpath
+import json
+
+def get_token():
+    f = open("token.txt", "r")
+    return f.read()
+
+def uploadCSV(token, csvpathtofile):
+    print("\nRequesting access to Zenodo...")
+    while True:
+        r = requests.get('https://zenodo.org/api/deposit/depositions', params = {'access_token': token})
+        if r.status_code == 200:
+            break
+
+    print("Granted!")
+    print("\nPreparing upload...")
+    headers = {"Content-Type": "application/json"}
+    while True:
+        r = requests.post('https://zenodo.org/api/deposit/depositions',
+            params={'access_token': token}, json={},
+            headers=headers)
+        if r.status_code == 201:
+            break
+    print("Upload successfully prepared with ID={}".format(r.json()['id']))
+
+    print("\nUploading file...")
+    deposition_id = r.json()['id']
+    data = {'name': ntpath.basename(csvpathtofile)}
+    files = {'file': open(csvpathtofile, 'rb')}
+    while True:
+        r = requests.post('https://zenodo.org/api/deposit/depositions/%s/files' % deposition_id,
+            params = {'access_token': token}, data = data,
+            files = files)
+        if r.status_code == 201:
+            break
+    print("Upload successful")
+
+    print("\nUpdating metadata...")
+    data = {
+        'metadata': {
+            'title': 'Dataset de la práctica',
+            'upload_type': 'poster',
+            'description': 'Este fichero contiene un ejemplo real del tipo de datos que se pueden encontrar al ejecutar el scraper',
+            'creators': [{'name': 'Víctor Colomé y Carlos Marcos',
+                          'affiliation': 'UOC'}]
+        }
+    }
+    while True:
+        r = requests.put('https://zenodo.org/api/deposit/depositions/%s' % deposition_id,
+            params={'access_token': token}, data=json.dumps(data),
+            headers=headers)
+        if r.status_code == 200:
+            break
+        print(r.status_code)
+        print(r.text)
+    print("Metadata successfully updated")
+
+    print("\nPublishing file...")
+    while True:
+        r = requests.post('https://zenodo.org/api/deposit/depositions/%s/actions/publish' % deposition_id,
+            params={'access_token': token} )
+        if r.status_code == 202:
+            break
+    print(r.text)
+    print("File successfully published with link {}".format(r.json()["files"][0]["links"]["download"]))
+
+if __name__ == '__main__':
+    accesstoken = get_token()
+    uploadCSV(accesstoken, "../sample/csv/adaptador-usb_articles_attributes_20200406.csv")
